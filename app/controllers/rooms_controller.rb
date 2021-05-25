b4fa3e5bc3022c7dacd4ef47068278ce5aefeedc
ShowWind846 <14k2132.sos@gmail.com>
2021-05-25 09:06:58 +0000

    [Fix]DeviseErrorMessageJa

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -4,6 +4,11 @@ class RoomsController < ApplicationController
     @room = Room.find(params[:id])
     @user = User.find(@room.relationship.user_id)
     @corp = Corp.find(@room.relationship.corp_id)
+    if user_signed_in?
+      @message_author = current_user
+    elsif corp_signed_in?
+      @message_author = current_corp
+    end
     @messages = Message.where(room_id: @room.id).order(created_at: :desc).page(params[:page]).per(10)
   end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @posts = Post.where(user_id: @user.id).order(created_at: :desc).page(params[:page]).per(5)
   end
 
   def mypage
